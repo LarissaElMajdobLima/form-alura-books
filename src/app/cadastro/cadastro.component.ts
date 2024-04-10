2e481a3d8f8713f78d77ed9ab47dd34346e7ff13
larissaElMajdobLima <larissa.lima@bandtec.com.br>
2024-04-10 00:33:41 -0300

    FEAT: Consuming API data

--- src/app/cadastro/cadastro.component.ts
+++ src/app/cadastro/cadastro.component.ts
@@ -1,6 +1,7 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { ConsultaCepService } from '../services/consulta-cep.service';
 
 @Component({
   selector: 'app-cadastro',
@@ -9,7 +10,12 @@ import { NgForm } from '@angular/forms';
 })
 export class CadastroComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  endereco: string = "";
+
+  constructor(
+      private router: Router,
+      private consultaCepService: ConsultaCepService
+    ){ }
 
   ngOnInit(): void {
   }
@@ -20,6 +26,24 @@ export class CadastroComponent implements OnInit {
     } else {
       alert('Formulário inválido');
     }
-    console.log(form);
+  }
+
+  consultarCep(ev: any, f: NgForm) {
+    const cep = ev.target.value;
+    
+    if(cep) {
+      this.consultaCepService.getConsultaCep(cep).subscribe((resultado) => {
+        this.popularEndereco(resultado, f);
+      }); 
+    }
+  }
+
+  popularEndereco(dados: any, f: NgForm) {
+    f.form.patchValue({
+      endereco: dados.logradouro,
+      bairro: dados.bairro,
+      cidade: dados.localidade,
+      estado: dados.uf
+    })    
   }
 }
